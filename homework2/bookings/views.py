3a1d18d354d3ef144066442dcd22e8a73e488a23
JJ17de <joshjones0530@icloud.com>
2026-03-03 21:10:58 +0000

    added seat and booking viewsets

--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Movie
 from .serializers import MovieSerializer
@@ -7,3 +6,11 @@ from .serializers import MovieSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
