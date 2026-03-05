d06a74e09a5f704c3374a8dbc8be5debb4d8ff9d
JJ17de <joshjones0530@icloud.com>
2026-03-05 02:56:23 +0000

    added unit tests for movie model

--- homework2/bookings/tests.py
+++ homework2/bookings/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Movie
+from datetime import date
 
-# Create your tests here.
+
+class MovieModelTest(TestCase):
+
+    def setUp(self):
+        self.movie = Movie.objects.create(
+            title="F1 The Movie",
+            description="one of the greatest movies of all time",
+            release_date=date(2025, 6, 27),
+            duration=155
+        )
+
+    def test_movie_creation(self):
+        self.assertEqual(self.movie.title, "F1 The Movie")
+        self.assertEqual(self.movie.duration, 155)
+
+    def test_movie_str(self):
+        self.assertEqual(str(self.movie), "F1 The Movie")
